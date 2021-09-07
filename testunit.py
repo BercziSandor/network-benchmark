001923a6e835bd3ebd3ae0e99bfe5450a559b369
Berczi, Sandor <Sandor.Berczi@t-systems.com>
2021-09-07 21:51:08 +0200

    update requirements

--- testunit.py
+++ testunit.py
@@ -1,25 +1,25 @@
 import unittest
 
-import network_test
+import network_benchmark
 
 
 class PingTest(unittest.TestCase):
 
     def testPingNoDNS(self):
-        assert network_test.LatencyBenchmark('8.8.8.8') is not None
+        assert network_benchmark.LatencyBenchmark('8.8.8.8') is not None
 
     def testPingNoDNSCustomTimeout(self):
-        assert network_test.LatencyBenchmark('8.8.8.8', 300) is not None
+        assert network_benchmark.LatencyBenchmark('8.8.8.8', 300) is not None
 
     def testPingDNS(self):
-        assert network_test.LatencyBenchmark('google.com') is not None
+        assert network_benchmark.LatencyBenchmark('google.com') is not None
 
     def testPingDNSCustomTimeout(self):
-        assert network_test.LatencyBenchmark('google.com', 300) is not None
+        assert network_benchmark.LatencyBenchmark('google.com', 300) is not None
 
     def testPingWrongDNS(self):
         try:
-            assert network_test.LatencyBenchmark('google.cx') is not None
+            assert network_benchmark.LatencyBenchmark('google.cx') is not None
         except IndexError:
             pass
         except Exception as e:
@@ -27,7 +27,7 @@ class PingTest(unittest.TestCase):
 
     def testPingWrongNoDNS(self):
         try:
-            assert network_test.LatencyBenchmark('256.232.111.0') is not None
+            assert network_benchmark.LatencyBenchmark('256.232.111.0') is not None
         except IndexError:
             pass
         except Exception as e:
