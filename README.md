ca7b65b9d13f74ce02442413ccbc57f60ca265fa
Berczi, Sandor <Sandor.Berczi@t-systems.com>
2021-09-07 21:25:27 +0200

    initial

--- README.md
+++ README.md
@@ -1,19 +1,2 @@
 # network-benchmark
-**This piece of code is originanly forked from FlipperPA: https://github.com/FlipperPA/latency-tester**
-
-[![Build Status](https://travis-ci.org/matthieu-lapeyre/network-benchmark.svg?branch=master)](https://travis-ci.org/matthieu-lapeyre/network-benchmark)
-
-You can quickly evaluate the latency of your network and obtain mean, std, percentile, timeout data. 
-Usage is `python network_test.py <ip> <n_sample> <timeout>` for example you can run:
-```console
-python network_test.py 192.168.0.1 50 1300
-```
-and the output will be something like this:
-
-``` console
-mean latency: 19.0138426854 ms
-std latency: 91.3184052069 ms
-99% latency: 7.4905 ms
-95% latency: 7.4905 ms
-timeout: 0.2 %
-```
+TODO
--- network_test.py
+++ network_test.py
@@ -1,63 +1,118 @@
+import datetime
+import time
+
 import numpy
-import pexpect
+from ping3 import ping
 
 
-class NetworkLatencyBenchmark(object):
-    def __init__(self, ip, timeout=1200):
+# based on: https://github.com/matthieu-lapeyre/network-benchmark
+
+class LatencyBenchmark(object):
+    def __init__(self, servers, interval_in_sec: float = 1.0, timeout_in_sec: float = 0.2):
         object.__init__(self)
 
-        self.ip = ip
-        self.interval = 0.5
+        self.servers = servers
+        self.interval_in_sec = interval_in_sec
 
-        ping_command = 'ping -i ' + str(self.interval) + ' ' + self.ip
-        self.ping = pexpect.spawn(ping_command)
+        self.timeout_in_sec = timeout_in_sec
 
-        self.ping.timeout = timeout
-        self.ping.readline()  # init
+        self.test_timestamps = []
+        self.network_latency = dict()
+        self.network_latency_np = dict()
+        self.network_timeout_count = dict()
+        self.network_timeout_percent = dict()
+        for server in servers:
+            alias = server.get("alias")
+            self.network_latency[alias] = []
+            self.network_latency_np[alias] = []
+            self.network_timeout_count[alias]: int = 0
+            self.network_timeout_percent[alias]: float = 0.0
 
-        self.wifi_latency = []
-        self.wifi_timeout = 0
-        self.print_status = True
+    def run_test(self, n_test):
+        for n in range(n_test):
+            ts = datetime.datetime.now()
+            self.test_timestamps.append(ts)
+            print(f'Test {n + 1}/{n_test} - {ts}')
+            for server in self.servers:
+                server_name = server.get('name')
+                server_alias = server.get('alias')
+                try:
+                    ping_time_orig = ping(server_name, timeout=1)
+                    if not ping_time_orig or ping_time_orig > self.timeout_in_sec:
+                        raise Exception
+                    ping_time_ms = round(float(ping_time_orig) * 1000, 0)
 
-    def run_test(self, n_sample=100):
-        for n in xrange(n_sample):
-            p = self.ping.readline()
+                except Exception as s:
+                    ping_time_ms = numpy.nan
+                    self.network_timeout_count[server_alias] += 1
 
-            try:
-                ping_time = float(p[p.find('time=') + 5:p.find(' ms')])
-                self.wifi_latency.append(ping_time)
-                if self.print_status:
-                    print 'test:', n + 1, '/', n_sample, ', ping latency :', ping_time, 'ms'
+                self.network_latency[server_alias].append(ping_time_ms)
+                self.network_timeout_percent[server_alias] = self.network_timeout_count[
+                                                                 server_alias] / \
+                                                             float(n + 1)
+                self.network_latency_np[server_alias] = numpy.array(
+                    self.network_latency[server_alias])
 
-            except:
-                self.wifi_timeout = self.wifi_timeout + 1
-                print 'timeout'
+                self.get_results(server_alias=server_alias, ping_time_ms=ping_time_ms)
 
-        self.wifi_timeout = self.wifi_timeout / float(n_sample)
-        self.wifi_latency = numpy.array(self.wifi_latency)
+            print(f'Waiting {self.interval_in_sec} seconds...\n')
+            time.sleep(self.interval_in_sec)
 
-    def get_results(self):
-        print 'mean latency:', numpy.mean(self.wifi_latency), 'ms'
-        print 'std latency:', numpy.std(self.wifi_latency), 'ms'
-        print '99% latency:', numpy.percentile(self.wifi_latency, 50), 'ms'
-        print '95% latency:', numpy.percentile(self.wifi_latency, 50), 'ms'
-        print 'timeout:', self.wifi_timeout * 100, '%'
+    def get_results(self, server_alias, ping_time_ms):
+        print(' {:<30} {}ms (x̄: {} ({}) ± {} ({}))'.format(
+            server_alias + ":", ping_time_ms,
+            numpy.nanmean(self.network_latency_np[server_alias]).round(decimals=2),
+            numpy.nanmean(self.network_latency_np[server_alias][-3:]).round(decimals=2),
+            numpy.nanstd(self.network_latency_np[server_alias]).round(decimals=2),
+            numpy.nanstd(self.network_latency_np[server_alias][-3:]).round(decimals=2)
+        ))
 
 
 if __name__ == '__main__':
-    import sys
+    servers = list()
+    servers = [
+        {
+            'name': 'tmv2626.devlab.de.tmo',
+            'alias': 'build server'
+        },
+        {
+            'name': 'rbe-wbench.workbench.telekom.de',
+            'alias': 'workbench'
+        },
+        {
+            'name': 'jira.devops.telekom.de',
+            'alias': 'Jira'
+        },
+        {
+            'name': 'qdehxd.de.t-internal.com',
+            'alias': 'RBE ABN APP'
+        },
+        {
+            'name': 'qdehxe.de.t-internal.com',
+            'alias': 'RBE ABN API'
+        },
+        {
+            'name': 'qderxx.de.t-internal.com',
+            'alias': 'RBE P3 API'
+        },
+        {
+            'name': 'qderxy.de.t-internal.com',
+            'alias': 'RBE P3 APP'
+        },
+        {
+            'name': 'tmv2331.devlab.de.tmo',
+            'alias': 'RBE Preint APP'
+        },
+        {
+            'name': 'QDEC2N.de.t-internal.com',
+            'alias': 'RPM Repo'
+        },
+        {
+            'name': 'telekom.sharepoint.de',
+            'alias': 'Sharepoint'
+        },
 
-    if len(sys.argv) < 3:
-        print "usage: python network_latency_test.py <ip> <n_sample> <timeout>"
-        sys.exit(1)
+    ]
 
-    ip = sys.argv[1]
-    n_sample = int(sys.argv[2]) 
-    timeout = int(sys.argv[3]) 
-    
-
-    network = NetworkLatencyBenchmark(ip,timeout)
-    network.print_status = False
-
-    network.run_test(n_sample)
-    network.get_results()
+    test = LatencyBenchmark(servers=servers, interval_in_sec=2) \
+            test.run_test(50)
