1470e955e96e668db796e51d9e34544ded7f3f5c
Berczi, Sandor <Sandor.Berczi@t-systems.com>
2021-09-09 12:59:58 +0200

    move of source

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='network-benchmark',
+    version='0.1',
+    packages=['test'],
+    url='',
+    license='',
+    author='Berczi, Sándor',
+    author_email='Berczi.Sandor@gmail.com',
+    description='Network benchmark tool'
+)
